a04a10a9523164c06a6cac1f604571487cb0cc3d
Prasanth <kesavarapu.siva@gmail.com>
2023-01-07 03:52:04 +0000

    don't wait until new line token

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,29 @@ use std::process::Stdio;
 use tokio::fs::OpenOptions;
 use tokio::io::stdin;
 use tokio::io::stdout;
-use tokio::io::AsyncBufReadExt;
-use tokio::io::AsyncWrite;
+use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::io::BufReader;
-use tokio::join;
 use tokio::process::Command;
 
-async fn write_to_both<S1: AsyncWrite + Unpin, S2: AsyncWrite + Unpin>(
-    s1: &mut S1,
-    s2: &mut S2,
-    bytes: &[u8],
-) -> Result<(), Box<dyn Error>> {
-    let (res1, res2) = join!(s1.write_all(&bytes), s2.write_all(&bytes));
-    res1?;
-    res2?;
-    Ok(())
+macro_rules! copy {
+    ($input:expr, $output:expr, $output2:expr) => {
+        async {
+            loop {
+                let mut buf = [0; 4096];
+                let count = $input.read(&mut buf).await?;
+                let res = tokio::join!(
+                    $output.write_all(&buf[0..count]),
+                    $output2.write_all(&buf[0..count])
+                );
+                res.0?;
+                res.1?;
+            }
+        }
+    };
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
     let command = std::env::var("RUN_COMMAND").expect("set command to run in `RUN_COMMAND`");
     let input_file = std::env::var("INPUT_FILE").unwrap_or("input".to_string());
@@ -44,22 +48,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut current_stdin = BufReader::new(stdin());
     let mut current_stdout = stdout();
 
-    let mut child_stdout_line = String::new();
-    let mut stdin_line = String::new();
-
-    loop {
-        tokio::select! {
-            out = child_stdout.read_line(&mut child_stdout_line)=>{
-                out?;
-                write_to_both(&mut current_stdout, &mut output_file, child_stdout_line.as_bytes()).await?;
-                child_stdout_line.clear();
-            }
-            out = current_stdin.read_line(&mut stdin_line)=>{
-                out?;
-                write_to_both(&mut input_file, &mut child_stdin, stdin_line.as_bytes()).await?;
-                stdin_line.clear();
-            }
-
-        }
+    tokio::select! {
+        r = copy!(child_stdout, current_stdout, output_file)=> r,
+        r = copy!(current_stdin, child_stdin, input_file)=> r,
     }
 }
